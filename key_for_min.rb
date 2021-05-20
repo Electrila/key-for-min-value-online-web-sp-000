018b31df7f4d29eac5c80ff2b1e374d83ee68404
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-20 16:27:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  key_smallest_value = ""
+  key_smallest_value = nil
   if name_hash == {}
     nil
   else
